eb378782ce2a91ae5106e94e26b13e8aa3a7ab75
Zachary Taylor <wakuwakuzakku@gmail.com>
2023-09-13 22:53:11 +0900

    deleted unused js and slowed gallery scroll

--- script.js
+++ script.js
@@ -11,25 +11,3 @@ closeBtn.addEventListener('click', () => {
     navLinks.classList.remove('active');
 });
 
-// Photo Gallery Scroll
-// const galleryContainer = document.querySelector('.gallery-container');
-// const galleryImages = galleryContainer.querySelectorAll('.gallery-image');
-
-// let currentImageIndex = 0;
-
-// function showImage(index) {
-//     galleryImages.forEach((image, idx) => {
-//         if (idx === index) {
-//             image.style.opacity = '1';
-//         } else {
-//             image.style.opacity = '0';
-//         }
-//     });
-// }
-
-// function nextImage() {
-//     currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
-//     showImage(currentImageIndex);
-// }
-
-// setInterval(nextImage, 3000); // Auto-scroll every 3 seconds
