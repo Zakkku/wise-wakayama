d34825b0628a4c31ddbdcf0641d78e5cd3c68a65
Zachary Taylor <wakuwakuzakku@gmail.com>
2023-09-25 13:12:05 +0900

    fixed read more button

--- script.js
+++ script.js
@@ -43,18 +43,48 @@ function showSlides(n) {
 
 // Read more button
 
+// function readMoreButton() {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myBtn");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more"; 
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less"; 
+//     moreText.style.display = "inline";
+//   }
+// }
+
 function readMoreButton() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  var btn = document.getElementById("myBtn");
 
   if (dots.style.display === "none") {
+    // Button is in the "Read more" state
     dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
+    btn.innerHTML = "Read more";
+    btn.classList.remove("clicked"); // Remove the "clicked" class
+    // Add your CSS styles for "Read more" state (white background, grey border)
+    btn.style.backgroundColor = "white";
+    btn.style.color = "black";
+    btn.style.border = "2px solid #555555";
     moreText.style.display = "none";
   } else {
+    // Button is in the "Read less" state
     dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
+    btn.innerHTML = "Read less";
+    btn.classList.add("clicked"); // Add the "clicked" class
+    // Add your CSS styles for "Read less" state (grey colored background)
+    btn.style.backgroundColor = "#555555";
+    btn.style.color = "white";
+    btn.style.border = "none";
     moreText.style.display = "inline";
   }
 }
+
+
